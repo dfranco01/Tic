d0bfd2f40e15c7f599f5b11795332196049e1e5f
David Franco <david.d.franco28@gmail.com>
2026-01-20 00:51:31 -0800

    board functionality established

--- TicTacToe.py
+++ TicTacToe.py
@@ -4,22 +4,25 @@ from player import Player
 from render import Board
 
 def main():
-    print("Welcome to TicTacToe Fucker")
+    print("Welcome to TicTacToe Fuckers")
     caller = Board()
 
+    print("Board is as follows, select numbers 1-9 to make a move")
     caller.render_board("_")
-    print("-" * 50)
-    caller.render_board("O", 1)
+    print("Player 1 is X, Player 2 is O")
 
 
-    sp = input("Input 1 to begin. All other input is invalid. Game automatically ends when one player wins.")
+    sp = input("Input 1 to begin. All other input is invalid. Game automatically ends when one player wins: ")
     if sp.isdigit() and int(sp) == 1:
         print("Step off")
         
         p1 = Player(1)
         p2 = Player(2)
         while True:
-            pass
+            choice = input("Player 1 make your move: ")
+            caller.render_board("X", int(choice))
+            choice = input("Player 2 make your move: ")
+            caller.render_board("O", int(choice))
     else:
         print("Terminating")
         exit()
--- render.py
+++ render.py
@@ -1,63 +1,73 @@
 #import TicTacToe
 
 class Board:
-    def render_board(self, symbol, *selection):
-        board = [
+    main_board = [
             ["#", "#", "#"],
             ['#', '#', '#'],
             ["#", "#", "#"]
         ]
+    def render_board(self, symbol, *selection):
+        board = self.main_board
         if symbol == "O":
             match selection[0]:
                 case 1:
-                    print(f"{symbol} | {board[0][1]} | {board[0][2]}")
+                    board[0][0] = symbol
+                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
                     print("--+---+--")
                     print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
                     print("--+---+--")
                     print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
                 case 2:
+                    board[0][1] = symbol
                     print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
                     print("--+---+--")
                     print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
                     print("--+---+--")
                     print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
                 case 3:
+                    board[0][2] = symbol
                     print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
                     print("--+---+--")
                     print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
                     print("--+---+--")
                     print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
                 case 4:
+                    board[1][0] = symbol
                     print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
                     print("--+---+--")
-                    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
+                    print(f"{[1][0]} | {board[1][1]} | {board[1][2]}")
                     print("--+---+--")
                     print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
                 case 5:
+                    board[1][1] = symbol
                     print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
                     print("--+---+--")
                     print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
                     print("--+---+--")
                     print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
                 case 6:
+                    board[1][2]
                     print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
                     print("--+---+--")
                     print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
                     print("--+---+--")
                     print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
                 case 7:
+                    board[2][0] = symbol
                     print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
                     print("--+---+--")
                     print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
                     print("--+---+--")
                     print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
                 case 8:
+                    board[2][1] = symbol
                     print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
                     print("--+---+--")
                     print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
                     print("--+---+--")
                     print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
                 case 9:
+                    board[2][2] = symbol
                     print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
                     print("--+---+--")
                     print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
@@ -69,57 +79,66 @@ class Board:
                     print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
                     print("--+---+--")
                     print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
-        else:
-            match selection:
+        if symbol == "X":
+            match selection[0]:
                 case 1:
+                    board[0][0] = symbol
                     print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
                     print("--+---+--")
                     print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
                     print("--+---+--")
                     print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
                 case 2:
+                    board[0][1] = symbol
                     print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
                     print("--+---+--")
                     print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
                     print("--+---+--")
                     print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
                 case 3:
+                    board[0][2] = symbol
                     print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
                     print("--+---+--")
                     print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
                     print("--+---+--")
                     print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
                 case 4:
+                    board[1][0] = symbol
                     print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
                     print("--+---+--")
                     print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
                     print("--+---+--")
                     print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
                 case 5:
+                    board[1][1] = symbol
                     print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
                     print("--+---+--")
                     print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
                     print("--+---+--")
                     print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
                 case 6:
+                    board[1][2] = symbol
                     print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
                     print("--+---+--")
                     print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
                     print("--+---+--")
                     print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
                 case 7:
+                    board[2][0] = symbol
                     print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
                     print("--+---+--")
                     print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
                     print("--+---+--")
                     print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
                 case 8:
+                    board[2][1] = symbol
                     print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
                     print("--+---+--")
                     print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
                     print("--+---+--")
                     print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
                 case 9:
+                    board[2][2] = symbol
                     print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
                     print("--+---+--")
                     print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
