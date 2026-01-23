0a8b5d0b057ce31028441f30e785ce8396d52bf9
David Franco <david.d.franco28@gmail.com>
2026-01-22 20:39:13 -0800

    Game complete

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,13 +1,14 @@
-# test
 from builtins import ValueError
 from render import Board
-
+            
 # game execution flow
 def main():
-    # NEXT STEPS
-    print("Welcome to TicTacToe Fuckers")
+    player1_choice = ""
+    player2_choice = ""
+    print("Welcome to TicTacToe!")
     caller = Board()
 
+    # Displaying empty board
     print("Board is as follows, select numbers 1-9 to make a move")
     caller.render_board("_")
     print("Player 1 is X, Player 2 is O")
@@ -17,13 +18,56 @@ def main():
     if sp.isdigit() and int(sp) == 1:
         print("Step off")
         
-        # production note, I may have the win check function only execute after one player has made at least 3 moves, in which case
-        # I might need a Player class, more to follow
         while True:
-            choice = input("Player 1 make your move: ")
-            caller.render_board("X", int(choice))
-            choice = input("Player 2 make your move: ")
-            caller.render_board("O", int(choice))
+            winner_found = False
+            player = "player 1"
+            player1_choice = input("Player 1 make your move: ")
+
+            #Ensuring player 1 chooses a proper position on the board from 1-9
+            while not player1_choice.isdigit() or int(player1_choice) < 1 or int(player1_choice) > 9:
+                print("Invalid selection, choose a number between 1 and 9")
+                player1_choice = input("Player 1 make your move: ")
+
+            #Ensuring player 1 doesn't choose an already occupied spot on the board
+            while(player1_choice == player2_choice):
+                print("That spot is already occupied, choose a vacant spot")
+                player1_choice = input("Player 2 make your move: ")
+            
+            caller.render_board("X", int(player1_choice))
+
+            # checking for a winner after player 1's move
+            for i in range(7):
+                if caller.win_check(i):
+                    winner_found = True
+                    break
+            if winner_found:
+                break
+
+            player = "player 2"
+            player2_choice = input("Player 2 make your move: ")
+
+            #Ensuring player 1 chooses a proper position on the board from 1-9
+            while not player2_choice.isdigit() or int(player2_choice) < 1 or int(player2_choice) > 9:
+                print("Invalid selection, choose a number between 1 and 9")
+                player2_choice = input("Player 1 make your move: ")
+
+            # ensuring player 2 doesn't choose an already occupied spot on the board
+            while(player1_choice == player2_choice):
+                print("That spot is already occupied, choose a vacant spot")
+                player2_choice = input("Player 2 make your move: ")
+            
+            caller.render_board("O", int(player2_choice))
+            
+            # checking for a winner after player 2's move
+            for i in range(7):
+                if caller.win_check(i):
+                    winner_found = True
+                    break
+            if winner_found:
+                break
+        
+        print(f'Congrats {player}, you have won')
+        exit()
     else:
         print("Terminating")
         exit()
--- render.py
+++ render.py
@@ -1,6 +1,7 @@
 #import TicTacToe
 
 class Board:
+    # board to be played on
     main_board = [
             ["#", "#", "#"],
             ['#', '#', '#'],
@@ -9,78 +10,8 @@ class Board:
     #this function displays the clean board to players at the start of match and after each move
     def render_board(self, symbol, *selection):
         board = self.main_board
+        # updating the baord with the appropraite symbol on the selected spot
         if symbol == "O":
-            match selection[0]:
-                case 1:
-                    board[0][0] = symbol
-                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
-                    print("--+---+--")
-                    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
-                    print("--+---+--")
-                    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
-                case 2:
-                    board[0][1] = symbol
-                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
-                    print("--+---+--")
-                    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
-                    print("--+---+--")
-                    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
-                case 3:
-                    board[0][2] = symbol
-                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
-                    print("--+---+--")
-                    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
-                    print("--+---+--")
-                    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
-                case 4:
-                    board[1][0] = symbol
-                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
-                    print("--+---+--")
-                    print(f"{[1][0]} | {board[1][1]} | {board[1][2]}")
-                    print("--+---+--")
-                    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
-                case 5:
-                    board[1][1] = symbol
-                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
-                    print("--+---+--")
-                    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
-                    print("--+---+--")
-                    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
-                case 6:
-                    board[1][2]
-                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
-                    print("--+---+--")
-                    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
-                    print("--+---+--")
-                    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
-                case 7:
-                    board[2][0] = symbol
-                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
-                    print("--+---+--")
-                    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
-                    print("--+---+--")
-                    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
-                case 8:
-                    board[2][1] = symbol
-                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
-                    print("--+---+--")
-                    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
-                    print("--+---+--")
-                    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
-                case 9:
-                    board[2][2] = symbol
-                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
-                    print("--+---+--")
-                    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
-                    print("--+---+--")
-                    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
-                case _:
-                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
-                    print("--+---+--")
-                    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
-                    print("--+---+--")
-                    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
-        if symbol == "X":
             match selection[0]:
                 case 1:
                     board[0][0] = symbol
@@ -151,9 +82,100 @@ class Board:
                     print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
                     print("--+---+--")
                     print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
+        elif symbol == "X":
+            match selection[0]:
+                case 1:
+                    board[0][0] = symbol
+                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
+                    print("--+---+--")
+                    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
+                    print("--+---+--")
+                    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
+                case 2:
+                    board[0][1] = symbol
+                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
+                    print("--+---+--")
+                    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
+                    print("--+---+--")
+                    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
+                case 3:
+                    board[0][2] = symbol
+                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
+                    print("--+---+--")
+                    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
+                    print("--+---+--")
+                    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
+                case 4:
+                    board[1][0] = symbol
+                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
+                    print("--+---+--")
+                    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
+                    print("--+---+--")
+                    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
+                case 5:
+                    board[1][1] = symbol
+                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
+                    print("--+---+--")
+                    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
+                    print("--+---+--")
+                    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
+                case 6:
+                    board[1][2] = symbol
+                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
+                    print("--+---+--")
+                    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
+                    print("--+---+--")
+                    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
+                case 7:
+                    board[2][0] = symbol
+                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
+                    print("--+---+--")
+                    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
+                    print("--+---+--")
+                    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
+                case 8:
+                    board[2][1] = symbol
+                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
+                    print("--+---+--")
+                    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
+                    print("--+---+--")
+                    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
+                case 9:
+                    board[2][2] = symbol
+                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
+                    print("--+---+--")
+                    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
+                    print("--+---+--")
+                    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
+                case _:
+                    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
+                    print("--+---+--")
+                    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
+                    print("--+---+--")
+                    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
+        else:
+            print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
+            print("--+---+--")
+            print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
+            print("--+---+--")
+            print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
+    
     #This function checks if a winner has been reached after each move
-    def board_check():
-        pass
-    #this function ensures the same spot on the board isn't chosen twice
-    def repeat_check():
-        pass
+    def win_check(self, win_scenario):
+        match win_scenario:
+            case 0:
+                return (Board.main_board[0][0] == 'X' or Board.main_board[0][0] == 'O') and (Board.main_board[0][0] == Board.main_board[1][0] == Board.main_board[2][0])
+            case 1:
+                return (Board.main_board[0][1] == 'X' or Board.main_board[0][1] == 'O') and (Board.main_board[0][1] == Board.main_board[1][1] == Board.main_board[2][1])
+            case 2:
+                return (Board.main_board[0][2] == 'X' or Board.main_board[0][2] == 'O') and (Board.main_board[0][2] == Board.main_board[1][2] == Board.main_board[2][2])
+            case 3:
+                return (Board.main_board[0][0] == 'X' or Board.main_board[0][0] == 'O') and (Board.main_board[0][0] == Board.main_board[0][1] == Board.main_board[0][2])
+            case 4:
+                return (Board.main_board[1][0] == 'X' or Board.main_board[1][0] == 'O') and (Board.main_board[1][0] == Board.main_board[1][1] == Board.main_board[1][2])
+            case 5:
+                return (Board.main_board[2][0] == 'X' or Board.main_board[2][0] == 'O') and (Board.main_board[2][0] == Board.main_board[2][1] == Board.main_board[2][2])
+            case 6:
+                return (Board.main_board[0][0] == 'X' or Board.main_board[0][0] == 'O') and (Board.main_board[0][0] == Board.main_board[1][1] == Board.main_board[2][2])
+            case 7:
+                return (Board.main_board[2][0] == 'X' or Board.main_board[2][0] == 'O') and (Board.main_board[2][0] == Board.main_board[1][1] == Board.main_board[0][2])
